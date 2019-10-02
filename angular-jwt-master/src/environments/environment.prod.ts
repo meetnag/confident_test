e8f5b99f747ecdcf491257d36cc5530a9398f15a
Nagesh K Deva <meetnag@yahoo.com>
2019-10-02 12:44:12 +0530

    No changes just a build for test server 100219

--- angular-jwt-master/src/environments/environment.prod.ts
+++ angular-jwt-master/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //apiUrl: 'http://api.testautotech.xyz/',
   // apiUrl :'http://localhost:3000/',
-  apiUrl: 'http://cdapi.lambdablocks.com/',
-  domainUrl: 'http://cd.lambdablocks.com/'
+  apiUrl: 'http://cdtestapi.lambdablocks.com/',
+  domainUrl: 'http://cdtest.lambdablocks.com/'
   //domainUrl: 'http://testautotech.xyz/'
 };
